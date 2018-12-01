7bcef3c17126b5b3b5c97d70f2a424058447d8d7
Alexandria Davis <alexandriadavis@csumb.edu>
2018-12-01 15:52:25 -0800

    Changed name of testing object to correct test

--- test/rnn_text_gen_test.py
+++ test/rnn_text_gen_test.py
@@ -5,8 +5,9 @@ import unittest
 from src.rnn_text_gen import RNN_Text_Gen
 
 
-class TestDataset(unittest.TestCase):
+class TestRNN(unittest.TestCase):
     def test_constructor(self):
         input_len = 10111
         unique_chars_count=10
         d1 = RNN_Text_Gen(input_len, unique_chars_count)
+
