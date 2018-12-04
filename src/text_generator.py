12a42e02b1c236ee51d1f9be91c2cba3ae9aeb3f
Donald Dong <xdong@csumb.edu>
2018-12-04 18:54:55 +0000

    Add a fully connected layer
    Fix #10 #12 #13
    
    - Remove redundant member variables
    - Prefix tensorflow operators with `tf_`

--- src/text_generator.py
+++ src/text_generator.py
@@ -10,39 +10,40 @@ class RNNTextGenerator:
         # RNN cell
         self.neurons_per_cell = 100
         self.name = 'RNNTextGenerator'
-        self.loss = None
-        self.input_place = None
-        self.output_place = None
-        self.rnn_cell = None
-        self.dynamic_rnn = None
-        self.state = None
-        self.optimizer = None
-        self.log_dir = None
+        self.tf_loss = None
+        self.tf_input = None
+        self.tf_target = None
+        self.tf_train = None
+        self.tf_logits = None
 
     def _build_tf_graph(self, seq_length, vocab_size):
+        """TF Graph Construction
+        """
         graph = tf.Graph()
         with graph.as_default():
-            self.input_place = tf.placeholder(
+            self.tf_input = tf.placeholder(
                 tf.float32, shape=(None, seq_length, 1)
             )
-            self.output_place = tf.placeholder(
+            self.tf_target = tf.placeholder(
                 tf.float32, shape=(None, seq_length, vocab_size)
             )
             with tf.variable_scope(self.name):
-                self.rnn_cell = tf.nn.rnn_cell.BasicRNNCell(
+                rnn_cell = tf.nn.rnn_cell.BasicRNNCell(
                     self.neurons_per_cell
                 )
-                # Dynamic RNN
-                self.dynamic_rnn, self.state = tf.nn.dynamic_rnn(
-                    self.rnn_cell, self.input_place, dtype=tf.float32
+                dynamic_rnn, states = tf.nn.dynamic_rnn(
+                    rnn_cell, self.tf_input, dtype=tf.float32
                 )
-                cost = tf.reduce_mean(
+                self.tf_logits = tf.layers.dense(states, vocab_size)
+                self.tf_loss = tf.reduce_mean(
                     tf.nn.softmax_cross_entropy_with_logits(
-                        logits=self.dynamic_rnn,
-                        labels=self.output_place
+                        logits=self.tf_logits,
+                        labels=self.tf_target
                     )
                 )
-                self.optimizer = tf.train.AdamOptimizer().minimize(cost)
+                self.tf_train = tf.train.AdamOptimizer().minimize(
+                    self.tf_loss
+                )
 
     def fit(self, inputs, targets, vocab_size):
         """Fit and train the classifier
