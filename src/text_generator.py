c1cb875c01f8e45013b945248383ac7caa7e7397
Donald Dong <account@donaldong.cn>
2018-12-07 01:12:43 -0800

    Add fit method (#17)
    Fix #4 #5 #15 #16
    
    - Fit the text generator
      - Create random dataset to fit the generator
    - Return accuracy and loss for each training batch
    - Pass the graph parameters in the constructor

--- src/text_generator.py
+++ src/text_generator.py
@@ -6,47 +6,108 @@ import tensorflow as tf
 class RNNTextGenerator:
     """A text generator using basic cell and dynamic rnn
     """
-    def __init__(self):
-        # RNN cell
-        self.neurons_per_cell = 100
-        self.name = 'RNNTextGenerator'
-        self.tf_loss = None
-        self.tf_input = None
-        self.tf_target = None
-        self.tf_train = None
-        self.tf_logits = None
+    def __init__(
+            self,
+            seq_length,
+            vocab_size,
+            neurons_per_cell,
+            name='RNNTextGenerator'
+    ):
+        """Initialize the text generator and contruct the tf graph
+        Arguments
+        ======================================================================
+        seq_length: int
+            The number of characters in a sequence.
 
-    def _build_tf_graph(self, seq_length, vocab_size):
-        """TF Graph Construction
+        vocab_size: int
+            The number of unique characters in the text.
+
+        neurons_per_cell: int
+            The number of neurons in each RNN cell.
+
+        name: string
+            The name of the net (for graph visualization in tensorboard).
         """
         graph = tf.Graph()
+        self.tf_sess = tf.Session(graph=graph)
         with graph.as_default():
+            # One-hot encoded input and targets
+            """placeholder
+            Example
+            [
+                batch_0: [
+                    seq_0: [
+                        # encoded labels with 5 categories
+                        [0, 0, 0, 1, 0],  # i = 0
+                        [0, 0, 1, 0, 0],  # i = 1
+                    ],
+                    ...
+                ],
+                ...
+            ]
+            """
             self.tf_input = tf.placeholder(
-                tf.float32, shape=(None, seq_length, 1)
+                tf.int32, shape=(None, seq_length, vocab_size)
             )
             self.tf_target = tf.placeholder(
-                tf.float32, shape=(None, seq_length, vocab_size)
+                tf.int32, shape=(None, seq_length, vocab_size)
             )
-            with tf.variable_scope(self.name):
-                rnn_cell = tf.nn.rnn_cell.BasicRNNCell(
-                    self.neurons_per_cell
+            with tf.variable_scope(name):
+                rnn_cell = tf.nn.rnn_cell.BasicRNNCell(neurons_per_cell)
+                outputs, _ = tf.nn.dynamic_rnn(
+                    rnn_cell,
+                    tf.cast(self.tf_input, tf.float32),
+                    dtype=tf.float32,
                 )
-                dynamic_rnn, states = tf.nn.dynamic_rnn(
-                    rnn_cell, self.tf_input, dtype=tf.float32
-                )
-                self.tf_logits = tf.layers.dense(states, vocab_size)
+                self.tf_logits = tf.layers.dense(outputs, vocab_size)
                 self.tf_loss = tf.reduce_mean(
                     tf.nn.softmax_cross_entropy_with_logits(
                         logits=self.tf_logits,
-                        labels=self.tf_target
+                        labels=self.tf_target,
                     )
                 )
                 self.tf_train = tf.train.AdamOptimizer().minimize(
                     self.tf_loss
                 )
+                self.tf_acc = tf.reduce_mean(tf.cast(
+                    tf.equal(
+                        tf.argmax(self.tf_logits, 1),
+                        tf.argmax(self.tf_target, 1),
+                    ),
+                    tf.float32
+                ))
+            # Initialize the tf session
+            self.tf_sess.run(tf.global_variables_initializer())
+            self.tf_sess.run(tf.local_variables_initializer())
 
-    def fit(self, inputs, targets, vocab_size):
-        """Fit and train the classifier
+    def fit(
+            self,
+            inputs,
+            targets,
+    ):
+        """Fit and train the classifier with a batch of inputs and targets
+        Arguments
+        ======================================================================
+        inputs: np.ndarray
+            A batch of input sequences.
+
+        targets: np.ndarray
+            A batch of target sequences.
+        """
+        self.tf_sess.run(
+            self.tf_train,
+            feed_dict={
+                self.tf_input: inputs,
+                self.tf_target: targets,
+            },
+        )
+        return self
+
+    def predict(self):
+        pass
+
+    def score(self, inputs, targets):
+        """Get the score for the batch
         Arguments
         ======================================================================
         inputs: np.ndarray
@@ -55,13 +116,18 @@ class RNNTextGenerator:
         targets: np.ndarray
             A batch of target sequences.
 
-        vocab_size: int
-            The number of unique characters in the text
+        Returns
+        ======================================================================
+        accuracy: tf.float32
+            The accuracy on this batch.
+
+        loss: tf.float32
+            The loss on this batch.
         """
-        self._build_tf_graph(inputs.shape[1], vocab_size)
-
-    def predict(self):
-        pass
-
-    def score(self):
-        pass
+        return self.tf_sess.run(
+            [self.tf_acc, self.tf_loss],
+            feed_dict={
+                self.tf_input: inputs,
+                self.tf_target: targets,
+            },
+        )
