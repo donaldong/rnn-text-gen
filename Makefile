a8fa777a4e9498d9ebadfb08d3e384fd3a63b0cf
Donald Dong <xdong@csumb.edu>
2018-12-07 22:34:09 +0000

    add test

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 .PHONY: unittest 
 
 unittest:
-	python -m unittest test/dataset_test.py
-	python -m unittest test/text_generator_test.py
+	python -m unittest test/dataset_test.py \
+		test/text_generator_test.py \
+		test/alice_test.py
--- test/dataset_test.py
+++ test/dataset_test.py
@@ -10,13 +10,6 @@ class TestDataset(unittest.TestCase):
     import tensorflow as tf
     tf.enable_eager_execution()
 
-    def test_constructor(self):
-        filename = 'data/alice.txt'
-        seq_length = 100
-        d1 = Dataset([filename], seq_length)
-        d2 = Dataset([filename] * 2, seq_length)
-        self.assertTrue(d1.text + d1.text == d2.text)
-
     def test_batch_a_seq(self):
         batch_size = 5
         seq_length = 100
