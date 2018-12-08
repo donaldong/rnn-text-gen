6716dc626431f9d0f35841ed4f9f2fcd8d8f2b8b
Donald Dong <xdong@csumb.edu>
2018-12-08 05:49:09 +0000

    Fix

--- test/alice_test.py
+++ test/alice_test.py
@@ -2,9 +2,6 @@
  ALICE'S ADVENTURES IN WONDERLAND
 """
 
-import tensorflow as tf
-tf.enable_eager_execution()
-
 import unittest
 import numpy as np
 from src.text_generator import RNNTextGenerator
@@ -15,19 +12,28 @@ class TestAlice(unittest.TestCase):
     def test_alice(self):
         seq_length = 25
         batch_size = 25
-        epoch = 5
+        learning_rate = 0.01
+        epoch = 10
         dataset = Dataset(['./data/alice.txt'], seq_length)
-        model = RNNTextGenerator(seq_length, dataset.vocab_size)
+        model = RNNTextGenerator(
+            seq_length,
+            dataset.vocab_size,
+            learning_rate=learning_rate
+        )
         for _ in range(epoch):
             for batch in dataset.batch(batch_size):
-                model.fit(batch['input'].numpy(), batch['target'].numpy())
-        start_seq = 'h'
-        text_gen = RNNTextGenerator(
+                model.fit(batch.inputs, batch.targets)
+        model.save()
+        start_seq = 'hello'
+        model = RNNTextGenerator(
             len(start_seq),
-            dataset.vocab_size
+            dataset.vocab_size,
         )
-        print('generated text: ', dataset.decode(RNNTextGenerator.sample(
-            text_gen,
-            dataset.encode(start_seq),
+        model.restore()
+        print('>>>>> {}'.format(start_seq), RNNTextGenerator.sample(
+            model,
+            dataset,
+            start_seq,
             50
-        )))
+        ))
+        print('<<<<<<')
