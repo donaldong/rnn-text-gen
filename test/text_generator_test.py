ef27a3b5e14722d4826270ea746fb8ab82cc6ee2
Donald Dong <xdong@csumb.edu>
2018-12-08 22:27:37 +0000

    Restore deleted lines
    When merging #38, some lines were deleted in `text_generator_test.py`

--- test/text_generator_test.py
+++ test/text_generator_test.py
@@ -3,6 +3,10 @@
 
 import unittest
 from src.text_generator import RNNTextGenerator
+
+
+class TestTextGenerator(unittest.TestCase):
+    def test_save_restore(self):
         print("---------------Testing text generator with randomly generated data-------------")
         seq_length = 10
         vocab_size = 4
