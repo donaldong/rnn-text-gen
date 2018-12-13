95d984283514c2333e5c1b6cb4d594c8631430ae
Donald Dong <xdong@csumb.edu>
2018-12-13 03:36:27 +0000

    Load new dataset `music.txt`
    - Improve the test case for dataset
    - Add `data/test.txt` for testing

--- test/dataset_test.py
+++ test/dataset_test.py
@@ -18,16 +18,38 @@ def test_batch(filename, batch_size, seq_length):
             for j in range(seq_length):
                 # One-hot encoded
                 assert sum(batch.inputs[i][j]) == 1
+                assert len(batch.inputs[i][j]) == dataset.vocab_size
+
 
 def test_sample(filename, batch_size, seq_length):
     dataset = Dataset([filename], seq_length)
-    print(dataset.sample(batch_size))
+    count = 0
+    batch = dataset.sample(batch_size)
+    for seq in batch.inputs:
+        assert len(seq) == seq_length
+        for i in range(seq_length):
+            # One-hot encoded
+            assert sum(seq[i]) == 1
+            assert len(seq[i]) == dataset.vocab_size
+        count += 1
+    assert count == batch_size
+
+
+def test_encode(filename, seq_length, text):
+    dataset = Dataset([filename], seq_length)
+    encoded = dataset.encode(text)
+    assert len(encoded) == len(text)
+    for label in encoded:
+        assert sum(label) == 1
+        assert len(label) == dataset.vocab_size
 
 class TestDataset(unittest.TestCase):
     def test(self):
         print("-----------Testing Dataset Module -----------")
         batch_size = 5
-        seq_length = 100
-        filename = 'data/alice.txt'
+        seq_length = 4
+        filename = 'data/test.txt'
         test_batch(filename, batch_size, seq_length)
         test_sample(filename, batch_size, seq_length)
+        test_encode(filename, seq_length, 'ab')
+
