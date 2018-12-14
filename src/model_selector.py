77cc907a32699c6803e3a73b38a86159542ce028
Donald Dong <xdong@csumb.edu>
2018-12-14 20:05:53 +0000

    Document the model_selector
    - Update README
    - Update the docstrings

--- src/model_selector.py
+++ src/model_selector.py
@@ -1,71 +1,119 @@
+"""Authors: Donald Dong
+"""
 import numpy as np
 import pandas as pd
 from src.text_generator import RNNTextGenerator
 
+
 class ModelSelector:
+    """Performs randomized search and rank the models by accuracy. It selects
+    the best ranking models and allows lengthy searching (for hours/days).
+    """
     def __init__(
             self,
             dataset,
             params,
             n_samples=5,
     ):
-        """Initialize the text generator and contruct the tf graph
+        """Creates a selector
+
         Arguments
         ======================================================================
         dataset: Dataset
-            The text dataset.
+            A `Dataset` object to train the model.
 
         params: dict
-            The entire search space.
+            A dictionary which describes the search space.  The each key of the
+            dictionary stores a list of parameters. The selector randomly
+            choice a parameter value from the list, for each parameter key.
 
         n_samples: int
-            The number of times to sample from the dataset for testing.
+            The number of times to sample from the dataset for testing. The
+            selector uses the average accuracy to rank the models.
         """
-        self.dataset = dataset
-        self.params = params
-        self.models = []
-        self.acc = []
-        self.loss = []
-        self.n_samples = n_samples
+        self._dataset = dataset
+        self._params = params
+        self._models = []
+        self._acc = []
+        self._loss = []
+        self._n_samples = n_samples
 
     def search(self):
-        """Perform a randomized search
+        """Search the parameter space. It generates a combination of
+        parameters, fit, and score the text generator. The selector keeps track
+        of the model and its average accuracy and score on the test data.
+
+        Returns
+        ======================================================================
+        model: RNNTextGenerator
+            A fitted `RNNTextGenerator`.
         """
         selected = {}
-        for name, options in self.params.items():
+        for name, options in self._params.items():
             selected[name] = np.random.choice(options)
         model = RNNTextGenerator(
-            self.dataset.seq_length,
-            self.dataset.vocab_size,
+            self._dataset.seq_length,
+            self._dataset.vocab_size,
             **selected
         )
-        model.fit(self.dataset)
+        model.fit(self._dataset)
         acc, loss = model.score(
-            self.dataset,
-            n_samples=self.n_samples
+            self._dataset,
+            n_samples=self._n_samples
         )
-        self.models.append(model)
-        self.acc.append(acc)
-        self.loss.append(loss)
+        self._models.append(model)
+        self._acc.append(acc)
+        self._loss.append(loss)
         return model
 
     def as_df(self):
-        """Save as a pandas Dataframe
+        """Save the searching result (models and their scores) as a pandas data
+        frame.
+        ```
+                                                   model  accuracy        loss
+    0  {'vocab_size': 70, 'rnn_cell': <class 'tensorf...  0.094519   88.173103
+    1  {'vocab_size': 70, 'rnn_cell': <class 'tensorf...  0.068282  104.829025
+    2  {'vocab_size': 70, 'rnn_cell': <class 'tensorf...  0.052424   12.201582
+        ```
+
+        Returns
+        ======================================================================
+        df: pd.DataFrame
+            A `pd.DataFrame` sorted by `accuracy` in non-increasing order.
         """
         return pd.DataFrame({
-            'model': self.models,
-            'accuracy': self.acc,
-            'loss': self.loss,
+            'model': self._models,
+            'accuracy': self._acc,
+            'loss': self._loss,
         }).sort_values(
             by=['accuracy'],
             ascending=False,
         )
 
+    def best_models(self, n):
+        """Get the model with the highest accuracy. It wraps the `best_models`
+        method.
+
+        Arguments
+        ======================================================================
+        n: int
+            The numer of best models.
+
+        Returns
+        ======================================================================
+        models: RNNTextGenerator[]
+            A list of `RNNTextGenerator` with length `n`.
+        """
+        return self.as_df().head(n)['model'].values[0]
+
     def best_model(self):
-        """Get the best model
+        """Get the model with the highest accuracy. It wraps the `best_models`
+        method.
+
         Returns
         ======================================================================
         model: RNNTextGenerator
-            The model with highest accuracy.
+            An `RNNTextGenerator` with the highest accuracy among the models
+            the selector has seen so far.
         """
-        return self.as_df().head(1)['model'].values[0]
+        return self.best_models(1)
